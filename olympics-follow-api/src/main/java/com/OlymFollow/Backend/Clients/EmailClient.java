fad060f068f6bb8e95f9f2bc3203e338bdec28d3
sergiocerq <sergiocerq11@gmail.com>
2024-08-22 23:09:17 -0300

    docs: adicionada documentação da chamada do serviço de email

--- olympics-follow-api/src/main/java/com/OlymFollow/Backend/Clients/EmailClient.java
+++ olympics-follow-api/src/main/java/com/OlymFollow/Backend/Clients/EmailClient.java
@@ -1,6 +1,7 @@
 package com.OlymFollow.Backend.Clients;
 
 import com.email.EmailOlympicsFollow.dtos.EmailDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,5 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface EmailClient {
 
   @PostMapping("/email/send")
+  @Operation(
+    summary = "Método de envio de email",
+    description = "Realiza a chamada para que o serviço de email envie um email com as informações da medalha cadastrada."
+  )
   public ResponseEntity<EmailDTO> sendEmail(@RequestBody EmailDTO emailDTO);
 }
